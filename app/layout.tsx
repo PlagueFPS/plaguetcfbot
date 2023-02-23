59b4dcde23296f8998e92cd0889c4537baf871fe
Plague <74983533+PlagueFPS@users.noreply.github.com>
2023-02-23 18:04:04 -0500

    Integrated next metadata object

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import './globals.css'
 import styles from './RootLayout.module.css'
-import { Rajdhani } from '@next/font/google'
+import { Metadata } from 'next';
+import { Rajdhani } from "next/font/google";
 import Navbar from '@/components/Navbar/Navbar'
 import Footer from '@/components/Footer/Footer'
-
 interface RootLayoutProps {
   children: React.ReactNode
 }
@@ -12,6 +12,31 @@ const rajdhani = Rajdhani({
   weight: ['300', '400', '500', '600', '700'],
   subsets: ['latin']
 })
+
+export const metadata: Metadata = {
+  creator: 'Plague',
+  icons: {
+    icon: '/images/favicon.png',
+    shortcut: '/images/favicon.png',
+    apple: '/images/favicon.png'
+  },
+  openGraph: {
+    url: 'https://main--plaguetcfbot.netlify.app',
+    siteName: 'PlagueTCFBot',
+    type: 'website',
+    images: [
+      {
+        url: '/images/favicon.png',
+        width: 300,
+        height: 300,
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary',
+    images: ['/images/favicon.png']
+  }
+}
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
 export const revalidate = 60
 import styles from './Home.module.css'
 import { use } from 'react'
+import { Metadata } from 'next'
 import { ContentfulCollection } from 'contentful'
 import { getPosts } from '@/utils/getPosts'
 import { TypeTcfbot} from '@/contentful/types'
 import Header from '@/components/Header/Header'
 import ContentContainer from '@/components/ContentContainer/ContentContainer'
 import CommandContainer from '@/components/CommandContainer/CommandContainer'
+
+export const generateMetadata = async () => {
+  const posts: ContentfulCollection<TypeTcfbot> = await getPosts({ content_type: 'tcfbot', 'sys.id': '1uGMNHezuD0HgDZGEcVeh3'})
+  const { title, description } = posts.items[0].fields
+  const metadata: Metadata = {
+    title: title,
+    description: description,
+    openGraph: {
+      title: title,
+      description: description
+    },
+    twitter: {
+      title: title,
+      description: description
+    }
+  }
+  return metadata
+}
 
 export default function Home() {
   const posts: ContentfulCollection<TypeTcfbot> = use(getPosts({ content_type: 'tcfbot', 'sys.id': '1uGMNHezuD0HgDZGEcVeh3'}))
